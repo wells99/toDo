cda6501045a7840d8d85b31c9cf8492a4505a4e8
wells99 <9.9wellington9.9@gmail.com>
2024-11-13 16:50:46 -0300

    atualização

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ var bodyy = document.getElementById("boddy");
 var count = Number(0)
 var botaoAdicionarTarefa = document.getElementById("tarefas");
 var listaDeTarefas = document.getElementById("lista-de-tarefas")
-
+var titulodaTarefa = "<h3 class='font-semibold'>Titulo do Texto</h3>";
+var textodaTarefa = "<p class='font-light'>texto texto texto <p/>"
+var classedaTarefa = '"mt-2 shadow-md rounded-md p-4 cursor-pointer shadow-gray-400 hover:bg-pink-500 hover:text-white hover:font-medium duration-300"'
 toogle.onclick = function (){
     if (count % 2 === Number(0)) {
 
@@ -23,6 +25,5 @@ toogle.onclick = function (){
 
 botaoAdicionarTarefa.onclick = function(){
     
-    
-    listaDeTarefas.innerHTML = " <li class= 'mt-2 shadow-md rounded-md p-4 cursor-pointer shadow-gray-400 hover:bg-pink-500 hover:text-white hover:font-medium duration-300'>Funcionando</li>"
+    listaDeTarefas.innerHTML = `<li class=${classedaTarefa}>${titulodaTarefa}${textodaTarefa}</li>`
 }
